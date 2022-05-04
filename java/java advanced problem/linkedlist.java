9d1638b50d57c559c980f1ccabd2b209c8e70f9c
shivam <tomar102003@gmail.com>
2022-05-04 19:13:28 +0530

    adding more advanced problem

--- java/java advanced problem/linkedlist.java
+++ java/java advanced problem/linkedlist.java
@@ -1,29 +1,57 @@
-import java.util.LinkedList;
-public class linkedlist {
-    public static void main(String[] args){
-        LinkedList<Integer> li=new LinkedList<>();
-        li.add(344);
-        li.add(567);
-        li.add(12);
-        for(int i=0;i<li.size();i++)
-        {
-            System.out.println(li.get(i));
-        }
-        // li.removeLast();
-        // li.add(2,4567);
-        try{
-            // int i=0;
-            if(li.size()<0)
-            {
-                throw new Exception();
-            }
-            li.getFirst();
-        }
-        catch(Exception e){
-            System.out.println("cannot answered");
-        }
+import java.util.Scanner;
 
-
+class Node{
+    int data ;
+    Node next;
+}
+public class LinkedList{
+    public static int  func1(Node n,int d){
+     if(n==null){
+        return 0;
+     }
+     else{
+         n.data=d;
+         n.next=null;
+         return 1;
+     }
+        
     }
-    
+    public static void printnode(Node h){
+        int u=1;
+        while(h.next!=null){
+            h=h.next;
+            System.out.print("The Data of "+u+" Node is:"+h.data+" ");
+            System.out.print("\n");
+        u++;
+        }
+        System.out.println("The total node is:"+(--u));
+    }
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("This program is used to create node and to displaying total nodes with its data");
+        
+        System.out.print("Enter the total no of nodes you want to create:");
+        int a=sc.nextInt();
+       
+        
+        Node head=new Node();
+        Node ptr =new Node();
+        ptr=head;
+        int p=1;
+        while(a>0){
+            Node n1=new Node();
+            ptr.next=n1;
+            System.out.print("Enter the value of " +p+" node");
+            int d=sc.nextInt();
+            func1(n1,d);
+            ptr=ptr.next;
+            p++;
+            a--;
+        }
+        //for printing the data of the node
+        printnode(head);
+
+        sc.close();
+    }
 }
